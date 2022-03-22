afaf54eff11fd93f8c63af94efdb6dda28b30a09
Muhammed2911 <muhammed.samir1423@gmail.com>
2022-03-22 21:50:20 +0200

    Init Project

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="homepage">
+    <the-header></the-header>
+  </div>
+</template>
+
+<script>
+// Importing Components
+import TheHeader from "../components/main/TheHeader.vue";
+export default {
+  name: "Home",
+  components: {
+    TheHeader,
+  },
+};
+</script>
